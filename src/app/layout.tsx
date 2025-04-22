706972035cea1a4ec9dcddfbdae793eee128072e
Mario <mario.gonzalez@live.u-tad.com>
2025-04-22 08:57:19 +0200

    Add basic Footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 export const metadata = {
   title: 'Web Tenis de Mesa',
@@ -10,9 +11,12 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es">
-      <body>
+      <body className="min-h-screen flex flex-col">
         <Header />
-        <main className="p-4">{children}</main>
+        <main className="flex-grow p-4">
+          {children}
+        </main>
+        <Footer />
       </body>
     </html>
   )
---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+export default function Footer() {
+  return (
+    <footer className="bg-gray-200 shadow-md mt-10 border-t border-gray-400">
+        <div className="max-w-7xl mx-auto px-4 py-4 text-center">
+          <p className="text-gray-600 text-sm">
+            © 2025 Tenis de Mesa. Todos los derechos reservados.
+          </p>
+        </div>
+
+    </footer>
+  )
+}
